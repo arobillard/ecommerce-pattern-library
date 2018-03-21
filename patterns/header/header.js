0f3e88a445670e26d7201c27cc862ae8535fbc76
arobillard <robi0653@algonquinlive.com>
2018-03-21 17:11:41 -0400

    Fix nav and link issues

--- patterns/header/header.js
+++ patterns/header/header.js
@@ -1,6 +1,7 @@
 // Nav Mobile --------------------
 
-$('.nav-label').on('click', function () {
+$('.nav-label').on('click', function (e) {
+  e.preventDefault();
   $(this).toggleClass('nav-label-open');
   $('nav').toggleClass('nav-open');
 });
