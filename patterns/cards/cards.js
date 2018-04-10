39a6dcec84e8c042ae353d00fc94a50888ffe72a
arobillard <robi0653@algonquinlive.com>
2018-04-09 21:48:10 -0400

    Upload complete checkout page and final patterns

--- patterns/cards/cards.js
+++ patterns/cards/cards.js
@@ -1,22 +1,25 @@
 // Delete Item From checkout
+var removeItem;
 
-$('.product-delete').on('click', function (e) {
+$('.delete-confirmed').on('click', function (e) {
   e.preventDefault();
-  var removeItem = $(this).attr('href');
-  var productName = $(this).parent().siblings('h3').text();
-  $('.delete-overlay').addClass('delete-confirm');
-  $('.delete-item-name').text(productName);
-  $('.delete-confirmed').focus();
-  $('.delete-confirmed').on('click', function (e) {
-    e.preventDefault();
-    $(removeItem).remove();
-    $('.delete-overlay').removeClass('delete-confirm');
-    $('main').focus();
-  });
+  $(removeItem).remove();
+  $('.delete-overlay').removeClass('delete-confirm');
+  $('main').focus();
 });
-
 $('.delete-confirm-close').on('click', function (e) {
   e.preventDefault();
   $('.delete-overlay').removeClass('delete-confirm');
   $('main').focus();
+});
+
+$('.product-delete').on('click', function (e) {
+  e.preventDefault();
+  removeItem = $(this).attr('href');
+  var productName = $(this).parent().siblings('h3').text();
+  $('.delete-overlay').addClass('delete-confirm');
+  $('.delete-item-name').text(productName);
+  $('.delete-confirmed').focus();
+
+  $(removeItem).removeData();
 });
